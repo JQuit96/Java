e23c2e685061a6a171122372b6df2a26bbae3dac
Julian D. Quitian <juliandquitian@yahoo.com>
2015-06-30 10:59:46 -0400

    Create HelloWorld.java

---
+++ Hello-World/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+  *Display a welcome message on the screen
+ *
+ */
+public class HelloWorld
+{
+   public static void main (String[] args)
+   {
+       System.out.println("Hello World!");
+       System.out.println("It is a great day for programming!");
+       System.out.println("I hope to contribute to the GitHub as much as possible!");
+
+    }//end of main method
+}//end of class
