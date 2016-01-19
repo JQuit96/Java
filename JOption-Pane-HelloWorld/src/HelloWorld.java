042ae1aad7de9826f60ebe0ca778f145d4eddabd
Julian D. Quitian <julian.quitian@gmail.com>
2016-01-18 22:09:36 -0500

    HelloWorld v1.0.0

---
+++ JOption-Pane-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Julian D. Quitian on 1/18/2016.
+ */
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello World of Java and message panels!");
+    }
+} // End class HelloWorld
